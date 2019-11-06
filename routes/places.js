c70cf20c24c61766626d40bfc16c997d43bce2a3
Malo Senpai <malorie.genoud@gmail.com>
2019-11-06 15:12:40 +0100

    GET place's id

--- routes/places.js
+++ routes/places.js
@@ -1,7 +1,10 @@
 const config = require('../config');
+const express = require('express');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+const utils = require('./utils');
 
-var express = require('express');
-var router = express.Router();
+const router = express.Router();
 
 // ------ Model ------
 const Place = require('../models/places');
@@ -19,8 +22,19 @@ router.get('/places', function(req, res, next) {
     });
 });
 
-router.get('/places/:id', function(req, res, next) {
-    res.send('place\'s profile');
+router.get('/places/:id', loadPlaceFromParamsMiddleware, function(req, res, next) {
+    //res.send('place\'s profile');
+
+    countPlacesBy(req.place, function(err, places) {
+        if (err) {
+            return next(err);
+        }
+
+        res.send({
+            ...req.place.toJSON(),
+            places
+        });
+    });
 });
 
 router.get('/places/:id/comments', function(req, res, next) {
@@ -63,5 +77,41 @@ router.delete('/places/:id', function(req, res, next) {
 router.delete('/places/:id/comments/:id', function(req, res, next) {
     res.send('delete place\'s comment');
 });
+
+// ------ FUNCTIONS ------
+/**
+ * Responds with 404 Not Found and a message indicating that the person with the specified ID was not found.
+ */
+function placeNotFound(res, placeId) {
+    return res.status(404).type('text').send(`No person found with ID ${placeId}`);
+}
+
+/**
+ * Given a person, asynchronously returns the number of movies directed by the person.
+ */
+function countPlacesBy(place, callback) {
+    Place.countDocuments().where('commentId', place._id).exec(callback);
+}
+
+// ------ MIDDLEWARES ------
+
+function loadPlaceFromParamsMiddleware(req, res, next) {
+
+    const placeId = req.params.id;
+    if (!ObjectId.isValid(placeId)) {
+        return placeNotFound(res, placeId);
+    }
+
+    Place.findById(req.params.id, function(err, place) {
+        if (err) {
+            return next(err);
+        } else if (!place) {
+            return placeNotFound(res, placeId);
+        }
+
+        req.place = place;
+        next();
+    });
+}
 
 module.exports = router;
